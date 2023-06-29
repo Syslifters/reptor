d3df0676f0aef3f71329765fd3ca32052a340c3d
rs <bschwabe@richardschwabe.de>
2023-06-29 20:37:51 +0300

    refactored reptor slightly, added typing

--- settings.py
+++ settings.py
@@ -1,7 +1,18 @@
 import pathlib
+from collections import OrderedDict
+from classes.ToolBase import ToolBase
+from classes.ConfBase import ConfBase
+from classes.UploadBase import UploadBase
 
 BASE_DIR = pathlib.Path(__file__).parent
 
 MODULE_DIRS = BASE_DIR / "modules"
 
 NEWLINE = '\n'
+
+SUBCOMMANDS_GROUPS = OrderedDict({
+    ConfBase: ('configuration', list()),
+    UploadBase: ('upload', list()),
+    ToolBase: ('tool output processing', list()),
+    'other': ('other', list()),
+})
