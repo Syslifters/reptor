de67a2478e1e7f05e131e21158355f847b2609d0
rs <bschwabe@richardschwabe.de>
2023-07-04 21:37:43 +0300

    Added community module flag in config

--- core/conf.py
+++ core/conf.py
@@ -13,6 +13,7 @@ class Config(ConfigProtocol):
         "server": "https://demo.sysre.pt",
         "token": None,
         "project_id": None,
+        "community": False,
     }
 
     def __new__(cls):
@@ -65,10 +66,12 @@ class Config(ConfigProtocol):
         self._raw_config["server"] = (
             input(f"Server [{default_server}]: ") or default_server
         )
+
         default_api_token = self._raw_config.get("token")
         self._raw_config["token"] = input(
             f"API Token{ f' [{default_api_token}]' if default_api_token else ''}: "
         ) or self._raw_config.get("token")
+
         default_project_id = self._raw_config.get("project_id")
         self._raw_config["project_id"] = (
             input(
@@ -76,13 +79,29 @@ class Config(ConfigProtocol):
             )
             or default_project_id
         )
+
+        default_community_enabled = self._raw_config.get("community")
+        is_community_enabled = "No"
+        if default_community_enabled:
+            is_community_enabled = "Yes"
+
+        self._raw_config["community"] = (
+            input(
+                f"Enable Community Plugins?{ f'[Currently: {is_community_enabled}]'} [yes/No]: "
+            )[:1].lower()
+            == "y"
+            or self._raw_config["community"]
+        )
+
         self.store_config()
 
     def store_config(self):
         """Offer user to write temporary config to local config file"""
         store = None
         while store not in ["y", "n"]:
-            store = input("Store to config to ~/.sysreptor/config.yaml? [y/n]: ")
+            store = input(
+                f"Store to config to {settings.PERSONAL_CONFIG_FILE}? [y/n]: "
+            )[:1].lower()
         if store == "y":
             self._write_to_file()
         self.instance._raw_config = self._raw_config
@@ -92,7 +111,10 @@ class Config(ConfigProtocol):
         if self._raw_config:
             settings.PERSONAL_SYSREPTOR_HOME.mkdir(exist_ok=True)
             with open(settings.PERSONAL_CONFIG_FILE, "w") as f:
-                yaml.dump(self._raw_config, f, encoding="utf-8")
+                filtered_config = {
+                    k: v for k, v in self._raw_config.items() if k not in ["cli"]
+                }
+                yaml.dump(filtered_config, f, encoding="utf-8")
         else:
             raise ValueError("No config is currently set")
 
