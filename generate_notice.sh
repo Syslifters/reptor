d6566ce425076cdf5154a14263f09289fcecb9da
aron <aron@syslifters.com>
2023-07-13 10:09:34 +0200

    Add notice generation script

---
+++ generate_notice.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+# Any subsequent(*) commands which fail will cause the shell script to exit immediately
+
+allow_only="MIT"
+allow_only="$allow_only;MIT License"
+allow_only="$allow_only;BSD License"
+allow_only="$allow_only;Apache Software License"
+allow_only="$allow_only;GNU Library or Lesser General Public License (LGPL)"
+allow_only="$allow_only;GNU Lesser General Public License v2 or later (LGPLv2+)"
+allow_only="$allow_only;GNU Lesser General Public License v3 (LGPLv3)"
+allow_only="$allow_only;Mozilla Public License 1.0 (MPL)"
+allow_only="$allow_only;Mozilla Public License 1.1 (MPL 1.1)"
+allow_only="$allow_only;Mozilla Public License 2.0 (MPL 2.0)"
+allow_only="$allow_only;Historical Permission Notice and Disclaimer (HPND)"
+allow_only="$allow_only;Python Software Foundation License"
+
+ignore="reptor"
+
+pip3 install pip-licenses
+pip-licenses --allow-only "$allow_only" >/dev/null
+pip-licenses -l --no-license-path -f plain-vertical --no-version --ignore-packages $ignore > NOTICE
