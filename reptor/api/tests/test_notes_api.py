42344916769f7feac1a5d28cdcabd93b014b19f8
aron <aron@syslifters.com>
2023-08-02 14:26:46 +0200

    Update template

---
+++ reptor/api/tests/test_notes_api.py
@@ -0,0 +1,49 @@
+import unittest
+
+from reptor.lib.reptor import Reptor
+from reptor.lib.errors import MissingArgumentError
+
+from ..NotesAPI import NotesAPI
+
+
+class TestNotesAPI(unittest.TestCase):
+    def setUp(self):
+        self.reptor = Reptor()
+
+    def test_notes_api_init(self):
+        # Test valid private note
+        self.reptor._config._raw_config['server'] = 'https://demo.sysre.pt'
+        self.reptor._config._raw_config['cli'] = {'private_note': True}
+        try:
+            n = NotesAPI(reptor=self.reptor)
+            self.assertTrue(n.private_note)
+        except (ValueError, MissingArgumentError):
+            self.fail("NotesAPI raised Error")
+
+        # Test valid project note
+        self.reptor._config._raw_config['server'] = 'https://demo.sysre.pt'
+        self.reptor._config._raw_config['cli'] = {'private_note': False}
+        self.reptor._config._raw_config['project_id'] = '2b5de38d-2932-4112-b0f7-42c4889dd64d'
+        try:
+            n = NotesAPI(reptor=self.reptor)
+            self.assertFalse(n.private_note)
+        except (ValueError, MissingArgumentError):
+            self.fail("NotesAPI raised Error")
+
+        # Test missing project id and missing private_note
+        self.reptor._config._raw_config['server'] = 'https://demo.sysre.pt'
+        self.reptor._config._raw_config['cli'] = {'private_note': False}
+        self.reptor._config._raw_config['project_id'] = ''
+        with self.assertRaises(MissingArgumentError):
+            NotesAPI(reptor=self.reptor)
+
+        # Test missing server
+        # self.reptor._config._raw_config['server'] = ''
+        # with self.assertRaises(ValueError):
+        #    NotesAPI(reptor=self.reptor)
+#
+        # Test missing project id
+        # self.reptor._config._raw_config['server'] = 'https://demo.sysre.pt'
+        # self.reptor._config._raw_config['project_id'] = ''
+        # with self.assertRaises(ValueError):
+        #    ProjectsAPI(reptor=self.reptor)
--- reptor/plugins/community/Sslyze/templates/summary.md
+++ reptor/plugins/community/Sslyze/templates/summary.md
@@ -3,12 +3,13 @@
 # {{ target.hostname }}:{{ target.port|unlocalize }} ({{ target.ip_address }})
 **Protocols**
 
-{% if 'sslv2' in target.protocols %}    * <span style="color: red">SSLv2</span>{% endif %}
-{% if 'sslv3' in target.protocols %}    * <span style="color: red">SSLv3</span>{% endif %}
-{% if 'tlsv1' in target.protocols %}    * <span style="color: red">TLS 1.0</span>{% endif %}
-{% if 'tlsv1_1' in target.protocols %}    * <span style="color: red">TLS 1.1</span>{% endif %}
-{% if 'tlsv1_2' in target.protocols %}    * <span style="color: red">TLS 1.2</span>{% endif %}
-{% if 'tlsv1_3' in target.protocols %}    * <span style="color: red">TLS 1.3</span>{% endif %}
+{% if 'sslv2' in target.protocols %}    * <span style="color: red">SSLv2</span>
+{% endif %}{% if 'sslv3' in target.protocols %}    * <span style="color: red">SSLv3</span>
+{% endif %}{% if 'tlsv1' in target.protocols %}    * <span style="color: red">TLS 1.0</span>
+{% endif %}{% if 'tlsv1_1' in target.protocols %}    * <span style="color: red">TLS 1.1</span>
+{% endif %}{% if 'tlsv1_2' in target.protocols %}    * <span style="color: red">TLS 1.2</span>
+{% endif %}{% if 'tlsv1_3' in target.protocols %}    * <span style="color: red">TLS 1.3</span>
+{% endif %}
 
 **Certificate Information**
 
