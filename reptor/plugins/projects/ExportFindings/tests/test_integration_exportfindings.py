561da87070066e8b4f7c191adfbfa56f79fffc52
aron <aron@syslifters.com>
2024-07-31 15:29:39 +0200

    Add test cases

--- reptor/plugins/projects/ExportFindings/tests/test_integration_exportfindings.py
+++ reptor/plugins/projects/ExportFindings/tests/test_integration_exportfindings.py
@@ -2,13 +2,15 @@ import json
 import subprocess
 
 import pytest
+import tomli
+import yaml
 
-from reptor.plugins.core.Conf.tests.conftest import projects_api, get_note
+from reptor.plugins.core.Conf.tests.conftest import get_note, projects_api
 
 
 @pytest.mark.integration
 class TestIntegrationExportFinding(object):
-    def test_render_project(self, projects_api):
+    def test_export_findings(self, projects_api):
         p = subprocess.Popen(
             ["reptor", "exportfindings", "--format", "json"],
             stdout=subprocess.PIPE,
@@ -16,7 +18,27 @@ class TestIntegrationExportFinding(object):
         )
         out, _ = p.communicate()
         assert p.returncode == 0
-        json.loads(out)
+        json_export = json.loads(out)
+
+        p = subprocess.Popen(
+            ["reptor", "exportfindings", "--format", "toml"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        out, _ = p.communicate()
+        assert p.returncode == 0
+        toml_export = tomli.loads(out.decode()).get("findings")  # TOML wraps in a dict
+
+        p = subprocess.Popen(
+            ["reptor", "exportfindings", "--format", "yaml"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        out, _ = p.communicate()
+        assert p.returncode == 0
+        yaml_export = yaml.safe_load(out.decode())
+
+        assert json_export == toml_export == yaml_export
 
         p = subprocess.Popen(
             ["reptor", "exportfindings", "--format", "json", "--upload"],
